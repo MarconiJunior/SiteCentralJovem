65302ab4fd81000558382371067d7673df7facf6
MarconiJunior <junior@brumafe.com>
2023-01-31 10:25:15 -0300

    Adicionando elementos

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,6 +13,8 @@ closeMenu.addEventListener('click', () => {
 })
 
 /*===== ACTIVE AND REMOVE MENU =====*/
+
+let currentIndex = 0
 const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
@@ -24,3 +26,47 @@ function linkAction() {
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+/* slideshow code */
+
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add("active");
+    setTimeout(showSlides, 3000);
+}
